c73239f337ee78078a32d3ddbcf1039c7f7ba328
zulfikar ilham <izulbustam@gmail.com>
2023-03-28 19:02:13 +0700

    Tugas ReactJS

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,7 +4,7 @@ import {Box} from './Box';
 export const Board = ({board, onClick}) => {
 
     return (
-        <div class='grid grid-cols-3 place-items-center flex justify-center mx-64'>
+        <div class='grid grid-cols-3 place-items-center justify-center mx-64'>
             {board.map((value, index) => {
                 return <Box value ={value} onClick={() => value === null && onClick(index)} />
             })}
